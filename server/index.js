a62aaf3d17c3d0fe55860ce842d977f9816771a9
Shivam Kumar <134121763+ShivamBokaria@users.noreply.github.com>
2025-09-02 03:15:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import fileRoutes from "./routes/fileRoutes.js";
@@ -10,26 +11,24 @@ import reportRoutes from "./routes/reportRoutes.js";
 import activityRoutes from "./routes/activityRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 
-
-
-
-
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json()); // for parsing JSON
-import cors from "cors";
+
 
 const allowedOrigins = [
-  "http://localhost:5173",                  // Vite dev server (local)
-  "https://excel1analyticsplatform.netlify.app/" // replace with your Netlify domain
+  "http://localhost:5173",                      // Vite dev server
+  "https://excel-analytics-platform.netlify.app" // Netlify deployed site
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
@@ -38,8 +37,6 @@ app.use("/api/reports", reportRoutes);
 app.use("/api/activity", activityRoutes);
 app.use("/api/ai", aiRoutes);
 
-
-
 // Connect to MongoDB
 connectDB();
 
@@ -47,7 +44,6 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
